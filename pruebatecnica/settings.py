98fdc2e3994dcc6aa91ef5269698e19f252e31c6
Javi9011 <javo9011@gmail.com>
2023-01-24 15:18:22 -0500

    Postgres

--- pruebatecnica/settings.py
+++ pruebatecnica/settings.py
@@ -30,6 +30,7 @@ DEBUG = 'RENDER' not in os.environ
 ALLOWED_HOSTS = []
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+
 if RENDER_EXTERNAL_HOSTNAME:   
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
@@ -83,10 +84,7 @@ WSGI_APPLICATION = 'pruebatecnica.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600
-    )
+    'default': dj_database_url.config()
 }
 
 
